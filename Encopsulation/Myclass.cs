f3f63e6a291a30e4abd96aafeb3b60e648d6dc15
ArmenHovhannisyan1998 <48031430+ArmenHovhannisyan1998@users.noreply.github.com>
2019-03-16 20:55:36 +0400

    Add project files.

---
+++ Encopsulation/Myclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encopsulation
+{
+    class Myclass
+    {
+        Myclass my = null;
+        private void Method()
+        {
+            Console.WriteLine("Hello C#");
+        }
+
+        public void CallMethod()
+        {
+            my = new Myclass();
+            my.Method();
+        }
+    }
+}
---
+++ Encopsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encopsulation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass my = new Myclass();
+            my.CallMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
